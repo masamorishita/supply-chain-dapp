553ef65d3cc0175e3e56325977dd18cd441c4f08
Masa Morishita <masam@masamorishita.com>
2019-07-02 10:56:11 +0900

    Fix some error on farvestItem function

--- contracts/coffeebase/SupplyChain.sol
+++ contracts/coffeebase/SupplyChain.sol
@@ -153,8 +153,15 @@ contract SupplyChain is FarmerRole, DistributorRole, RetailerRole, ConsumerRole 
   }
 
   // Define a function 'harvestItem' that allows a farmer to mark an item 'Harvested'
-  function harvestItem(uint _upc, address _originFarmerID, string _originFarmName, string _originFarmInformation, string  _originFarmLatitude, string  _originFarmLongitude, string  _productNotes) public 
-  {
+  function harvestItem(
+    uint _upc,
+    address _originFarmerID,
+    string memory _originFarmName,
+    string memory _originFarmInformation,
+    string memory _originFarmLatitude,
+    string memory _originFarmLongitude,
+    string memory _productNotes
+    ) public {
     // Add the new item as part of Harvest
     items[_upc].itemState = State.Harvested;
     items[_upc].originFarmerID = _originFarmerID;
