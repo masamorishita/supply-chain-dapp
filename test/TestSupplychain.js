9d2c5947af097784418776e3f86b75514274a64b
Masa Morishita <masam@masamorishita.com>
2019-07-11 22:20:42 +0900

    Modify 6th Test on TestSupplychain.js

--- test/TestSupplychain.js
+++ test/TestSupplychain.js
@@ -174,19 +174,22 @@ contract('SupplyChain', function(accounts) {
         const supplyChain = await SupplyChain.deployed()
         
         // Declare and Initialize a variable for event
-        
+        var eventEmitted = false
         
         // Watch the emitted event Shipped()
-        
+        supplyChain.Shipped((err, res) => {
+            eventEmitted = true;
+        })
 
         // Mark an item as Sold by calling function buyItem()
-        
+        await supplyChain.shipItem(upc, {from: distributorID})
 
         // Retrieve the just now saved item from blockchain by calling function fetchItem()
-        
+        const resultBufferTwo = await supplyChain.fetchItemBufferTwo.call(upc)
 
         // Verify the result set
-              
+        assert.equal(resultBufferTwo[5], 5, 'Error: Invalid item State')
+        assert.equal(eventEmitted, true, 'Invalid event emitted')              
     })    
 
     // 7th Test
